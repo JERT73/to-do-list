77118563ff6d9bc532bdd68775ab14d60862082f
aksrono7 <149877201+aksrono7@users.noreply.github.com>
2024-10-04 23:42:13 +0530

    scripts.js

---
+++ createaccountpage/scripts.js
@@ -0,0 +1,20 @@
+document.getElementById("createAccountBtn").addEventListener("click", function () {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    let successMessage = document.getElementById("success-message");
+
+    if (username === "" || password === "" || password !== confirmPassword) {
+        successMessage.textContent = "Passwords do not match.";
+        successMessage.style.display = "block";
+        successMessage.style.color = "darkred";
+    } else {
+        successMessage.textContent = "Account Created Successfully!";
+        successMessage.style.display = "block";
+        successMessage.style.color = "green";
+
+        setTimeout(() => {
+            window.location.href = "index.html"; // Redirect to Login Page
+        }, 1000);
+    }
+});
